77bd9e2e3ecbc28492f8d29a21e0ef5c0f251503
Ifenkiul <61749481+Ifenkiul@users.noreply.github.com>
2020-06-06 22:42:03 +0300

    fixes

--- js/myscript.js
+++ js/myscript.js
@@ -16,3 +16,23 @@ $(window).scroll(function () {
     nav.removeClass('f-nav');
   }
 });
+
+miniMenuBtnClick();
+/* MINI MENU BTN CLICK*/
+function miniMenuBtnClick() {
+  let miniLogoClicked = true;
+  const miniBtn = document.querySelector('.mini__btn');
+
+  miniBtn.addEventListener('click', function () {
+    document.querySelector('.mini__menu').classList.toggle('active');
+    if (miniLogoClicked == false) {
+      miniBtn.src = 'img/compass.png';
+      miniLogoClicked = true;
+    } else {
+      miniBtn.src = 'img/compass_closed.png';
+      miniLogoClicked = false;
+    }
+  });
+}
+
+/* MINI MENU BTN CLICK*/
