89250e84d4d7412b19079a7f92e2e19ed8d115b7
Ifenkiul <61749481+Ifenkiul@users.noreply.github.com>
2020-06-06 22:33:43 +0300

    fds

--- js/handbook_viewer.js
+++ js/handbook_viewer.js
@@ -7,3 +7,20 @@ async function fetchHtml() {
   const html = await response.text();
   document.querySelector('.main').innerHTML = html;
 }
+/* MINI MENU BTN CLICK*/
+
+let miniLogoClicked = true;
+const miniBtn = document.querySelector('.mini__btn');
+
+miniBtn.addEventListener('click', function () {
+  document.querySelector('.mini__menu').classList.toggle('active');
+  if (miniLogoClicked == false) {
+    miniBtn.src = 'img/compass.png';
+    miniLogoClicked = true;
+  } else {
+    miniBtn.src = 'img/compass_closed.png';
+    miniLogoClicked = false;
+  }
+});
+
+/* MINI MENU BTN CLICK*/
