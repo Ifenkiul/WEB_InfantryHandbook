a40f629be66b1f6aefc671b1480a279a009351e2
Ifenkiul <61749481+Ifenkiul@users.noreply.github.com>
2020-09-16 18:52:10 +0300

    fixes

--- js/handbook.js
+++ js/handbook.js
@@ -1,22 +1,5 @@
 'use strict';
 let previousOpen = 0;
-const linksList = {
-  'Явище пострілу': '',
-  'Розвідка цілей': 'vogneva/scouting_01.html',
-  'Кутові величини': 'vogneva/angles.html',
-  'Правила стрільби стрілецька зброя': 'vogneva/how_to_shoot_gun.html',
-  'АК-47М': 'vogneva/ak47.html',
-  'АК-74': 'vogneva/ak74.html',
-  'РПК-74': 'vogneva/rpk74.html',
-  'ПК (ПКМ)': 'vogneva/pk.html',
-  'РПГ-7В': 'vogneva/rpg.html',
-  'Ручні уламкові гранати': 'vogneva/grenades.html',
-  'ГП-25': 'vogneva/gp25.html',
-  ДШКМ: 'vogneva/dashka.html',
-  'НСВ Утес': 'vogneva/utes.html',
-  'АГС-17 Пламя': 'vogneva/ags17.html',
-  'СПГ-9 Копье': 'vogneva/spg9.html',
-};
 
 //---------------------HANDBOOK
 document
@@ -47,14 +30,11 @@ function handbookClick(event) {
 }
 
 // ЗАПИСУЮ В СХОВИЩЕ СЕССІЙНЕ ЯКУ САМЕ ТЕМУ ОБРАНО
-document
-  .querySelectorAll('.content__menu-link')
-  .forEach(link => link.addEventListener('click', pushInfo));
-
-function pushInfo(linkPressed) {
-  const linkText = linkPressed.target.innerText;
-  sessionStorage.setItem('page', linksList[linkText]);
-}
+document.querySelectorAll('.content__menu-link').forEach(link =>
+  link.addEventListener('click', linkPressed => {
+    sessionStorage.setItem('page', linkPressed.target.dataset.url);
+  }),
+);
 
 //--------------------------------LIBRARY
 document
