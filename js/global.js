6741eeae6c080c6eea83991ca1b2eeb096d907ca
Ifenkiul <ifenkiul@gmail.com>
2021-01-13 16:33:18 +0200

    IH-002

--- js/global.js
+++ js/global.js
@@ -7,3 +7,22 @@ burgerClick();
 navigatePage();
 changeToolbarColor();
 messageBox.showMessageBox();
+
+function changeTitle() {
+    const titlesArray = [
+        "Не переставай вчитись",
+        "Будь мужнім",
+        "Ділись знаннями",
+        "Не аватарь",
+        "Довідник піхотинця",
+        "Infantry handbook",
+        "Чисть зброю",
+        "Бережи екіпіровку",
+        "Вдосконалюйся",
+        "Не розслабляйся",
+        "Будь пильним"
+    ];
+    const index = Math.floor(Math.random() * 11);
+    document.title =titlesArray[index];
+}
+setInterval(changeTitle, 3000);
